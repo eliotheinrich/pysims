57c631aa7196a584dcc8ddab1087e19a11e095c7
Eliot Heinrich <eliot.heinrich@gmail.com>
2025-03-06 09:48:36 -0500

    Small change to num_runs keyword

--- pysims/job_controller.py
+++ pysims/job_controller.py
@@ -186,7 +186,7 @@ def submit_jobs(
         checkpoint_file=None,
         init_callback=None,
         cleanup=True,
-        num_runs=1,
+        nruns=1,
         memory="5gb",
         time="24:00:00",
         ncores=1,
@@ -207,7 +207,7 @@ def submit_jobs(
 
     metaparams = {
         "num_threads": ncores,
-        "num_runs": num_runs,
+        "num_runs": nruns,
 
         "atol": atol,
         "rtol": rtol,
